ef80c30568820fa5c8d62e8dc0a20e181544c3c3
GleysonSilva <gleysonmongodb@outlook.com>
2022-05-12 00:15:33 -0300

    comimt main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,7 +51,7 @@ export function Home() {
           alt="Ilustração simbolizando perguntas e respostas"
         />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p> */}
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>  */}
       </aside>
       <main>
         <div className="main-content">
